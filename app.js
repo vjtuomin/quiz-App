ac8686c243edb95aa75386a6550a7809f9d3c94d
vjtuomin <valtteri.j.tuominen@student.jyu.fi>
2021-12-21 18:21:50 +0200

    readme

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import { Application, Session } from "./deps.js";
 import { authMiddleware } from "./middlewares/authMiddleware.js";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 import renderMiddleware from "./middlewares/renderMiddleware.js";
-import { serveStaticMiddleware } from "./middlewares/serveStaticMiddleware.js";
 import { router } from "./routes/routes.js";
 
 const app = new Application();
